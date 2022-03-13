fa397d9f76c67375236a6b1d29ca25d7a37ba5f6
abdessamadbettal <abdessamadbettal@gmail.com>
2022-03-13 15:06:39 +0100

    final version of the calcul

--- calcul.js
+++ calcul.js
@@ -1,80 +1,39 @@
 // alert("js running");
-
 //? ----------------- variables -------------//
 var queue = [];
 var input = 0;
 var screen = document.getElementById("screen");
-// var numChar; //total number of types characters
-// var currentChar , previousChar ; //store current and previous taped
-// var operations = ['+' , '-' , '*' , '/'];
 
 //? ----------------- functions -------------//
-function caculerScreen(resultat) {
-  if (input !== 0) {
-    input = parseFloat(input);
-    AddToQueue(input);
-
-    console.log(`inputdans fonction calculer :   ${input}`);
-    console.log(`queue dans fonction calculer :   ${queue}`);
-  }
-  var answer = resultat[0];
-  var Diviser_zero = true;
-  for (var i = 2; i < resultat.length; i++) {
-    console.log(`queue[i-1] :   ${queue[i - 1]}`);
-    switch (queue[i - 1]) {
-      case "+":
-        answer += resultat[i];
-        console.log(`resultat[i] :   ${resultat[i]}`);
-        break;
-      case "-":
-        answer -= resultat[i];
-        console.log(`resultat[i] :   ${resultat[i]}`);
-        break;
-      case "*":
-        answer *= resultat[i];
-        console.log(`resultat[i] :   ${resultat[i]}`);
-        break;
-      case "/":
-        if (resultat[i] === 0) {
-          Diviser_zero = false ;
-        } else {
-          answer = answer / resultat[i];
-        }
-        console.log(`resultat[i] :   ${resultat[i]}`);
-        break;
-    }
-  }
-  if (Diviser_zero === false) {
-    clearScreen();
-    screen.value = "ERROR";
-    console.log(`the resultat is ${queue}`);
-  }
-  else{
-    screen.value = answer;
-    input = answer;
-    queue = [];
-    console.log(`the resultat is ${answer}`);
-  }
-
-}
+// console.log(typeof(screen));
+//! on affiche sur les chiffres dans screen
 function display(num) {
-  if (num != ".") {
+  if (screen.value === "ERROR" && num != ".") {
+    screen.value = "";
+  }
+  if (num != "." || !input.match(/[.]/)) {
     input += num;
     screen.value += num;
     console.log(`input lors de display : ${input}`);
   }
 }
+
+// ! on va suppermer les dannés dans le screen
 function clearScreen() {
   screen.value = "";
   queue = [];
   input = 0;
   console.log(`input lors de display : ${input}`);
 }
+
+// !! add input on the queue comme un array
 function AddToQueue(input) {
   queue.push(input);
 }
+
+// ! une fois cliquer sur les + , - , / , *
 function operation(arg) {
-  if (input !== 0) {
+  if (input !== 0 && input !== "-") {
     input = parseFloat(input);
     AddToQueue(input);
     AddToQueue(arg);
@@ -83,7 +42,74 @@ function operation(arg) {
     input = 0;
     console.log(`input lors de l'operation : ${input}`);
   }
+  if (arg == "-" && isNaN(queue[0]) && input !== "-") {
+    input = "-";
+    screen.value = "-";
+  }
 }
+
+// ! une fois cluquer sur = va calculer le résultat
+function caculerScreen(resultat) {
+  // * pour add input sur la queue
+  if (input !== 0 ) {
+    input = parseFloat(input);
+    AddToQueue(input);
+    console.log(`input dans fonction calculer :   ${input}`);
+    console.log(`queue dans fonction calculer :   ${queue}`);
+  }
+  var answer = resultat[0];
+  var Diviser_zero = true;
+  for (var i = 2; i < resultat.length; i++) {
+    switch (queue[i - 1]) {
+      case "+":
+        answer += resultat[i];
+        console.log(`queue[i-1] boucle:   ${queue[i - 1]}`);
+        console.log(`resultat[i] :   ${resultat[i]}`);
+        break;
+      case "-":
+        answer -= resultat[i];
+        console.log(`resultat[i] :   ${resultat[i]}`);
+        console.log(`queue[i-1] boucle:   ${queue[i - 1]}`);
+        break;
+      case "*":
+        answer *= resultat[i];
+        console.log(`resultat[i] :   ${resultat[i]}`);
+        console.log(`queue[i-1] boucle:   ${queue[i - 1]}`);
+        break;
+      case "/":
+        if (resultat[i] === 0) {
+          Diviser_zero = false;
+        } else {
+          answer = answer / resultat[i];
+          console.log(`resultat[i] :   ${resultat[i]}`);
+          console.log(`queue[i-1] boucle:   ${queue[i - 1]}`);
+        }
+        break;
+      default:
+        console.log(`the condition is not aviable`);
+        break;
+    }
+    console.log(`the inswer is :   ${answer}`);
+  }
+  if (Diviser_zero === false) {
+    clearScreen();
+    screen.value = "ERROR";
+    console.log(`the resultat is ${queue}`);
+  } else {
+    screen.value = answer;
+    input = answer;
+    queue = [];
+    console.log(`the resultat is ${answer}`);
+  }
+}
+
+// ! supermer lr dernier chiffre
+function supermer() {
+  screen.value = screen.value.substring(0, screen.value.length - 1);
+  input = input.substring(0, input.length - 1);
+}
+
+// ! pour tester le code sur le console
 function consoleTest() {
   console.log(`######## console en general ########`);
   console.log(`input en général : ${input}`);
@@ -91,6 +117,11 @@ function consoleTest() {
   // console.log(`add to queue input parsefloat :   ${AddToQueue(input)}`);
   console.log(`####################################`);
 }
+// || !input.match(/[.]/)
+
+// var numChar; //total number of types characters
+// var currentChar , previousChar ; //store current and previous taped
+// var operations = ['+' , '-' , '*' , '/'];
 
 // function clearScreen() {
 //   screen.value = "";
